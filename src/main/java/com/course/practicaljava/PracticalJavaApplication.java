c74e41e76f8f369d585b7ad27a9a099f5e0ea9a5
USER <timotius.pamungkas@gmail.com>
2018-10-16 00:15:03 +0700

    Add log to DefaultRestController
    Change System.out.println to Spring log

--- src/main/java/com/course/practicaljava/PracticalJavaApplication.java
+++ src/main/java/com/course/practicaljava/PracticalJavaApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class PracticalJavaApplication {
 
 	public static void main(String[] args) {
+		// Test
 		SpringApplication.run(PracticalJavaApplication.class, args);
 	}
 }
